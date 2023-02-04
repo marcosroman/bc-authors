dc75099246acca9d2c4ae86938d6c1454105db01
marcosroman <marcosdromand@gmail.com>
2023-02-04 16:51:38 -0300

    removed all css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import AppRoutes from './components/routes/AppRoutes';
 
 const App = () => {
--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-
-/*
-const path = require("path");
-
-module.exports = {
-  resolve: {
-    alias: {
-      Components: path.resolve(__dirname, "components"),
-    },
-  },
-};
-*/
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
